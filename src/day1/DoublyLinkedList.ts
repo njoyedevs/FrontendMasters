7a2023e82f5cefc8d5488955e9483dc24efba02c
Nicolas Joye <njoyedevs@gmail.com>
2023-05-09 20:50:07 -0700

    DoublyLinkedList, Stack

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,146 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+};
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
     }
 
     prepend(item: T): void {
+        const node: Node<T> = { value: item, next: this.head };
+        if (this.head) {
+            this.head.prev = node;
+        }
+        this.head = node;
+        if (!this.tail) {
+            this.tail = node;
+        }
+        this.length++;
+    }
 
-}
     insertAt(item: T, idx: number): void {
+        if (idx < 0 || idx > this.length) {
+            throw new Error("Index out of bounds");
+        }
+    
+        if (idx === 0) {
+            this.prepend(item);
+            return;
+        }
+    
+        if (idx === this.length) {
+            this.append(item);
+            return;
+        }
+    
+        let currentNode = this.head;
+        let currentIdx = 0;
+    
+        while (currentNode && currentIdx < idx) {
+            currentNode = currentNode.next;
+            currentIdx++;
+        }
+    
+        if (currentNode && currentNode.prev) {
+            const newNode: Node<T> = { value: item, next: currentNode, prev: currentNode.prev };
+            currentNode.prev.next = newNode;
+            currentNode.prev = newNode;
+            this.length++;
+        }
+    }
 
-}
     append(item: T): void {
+        const node: Node<T> = { value: item, prev: this.tail };
+        if (this.tail) {
+            this.tail.next = node;
+        }
+        this.tail = node;
+        if (!this.head) {
+            this.head = node;
+        }
+        this.length++;
+    }
 
-}
     remove(item: T): T | undefined {
+        let currentNode = this.head;
 
-}
+        while (currentNode) {
+            if (currentNode.value === item) {
+                if (currentNode.prev) {
+                    currentNode.prev.next = currentNode.next;
+                } else {
+                    this.head = currentNode.next;
+                }
+
+                if (currentNode.next) {
+                    currentNode.next.prev = currentNode.prev;
+                } else {
+                    this.tail = currentNode.prev;
+                }
+
+                this.length--;
+                return item;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        return undefined;
+    }
+
     get(idx: number): T | undefined {
+        if (idx < 0 || idx >= this.length) {
+            throw new Error("Index out of bounds");
+        }
 
-}
+        let currentNode = this.head;
+        let currentIdx = 0;
+
+        while (currentIdx < idx) {
+            currentNode = currentNode?.next;
+            currentIdx++;
+        }
+
+        return currentNode?.value;
+    }
+
     removeAt(idx: number): T | undefined {
-
-}
+        if (idx < 0 || idx >= this.length) {
+            throw new Error("Index out of bounds");
+        }
+    
+        let currentNode = this.head;
+        let currentIdx = 0;
+    
+        while (currentNode && currentIdx < idx) {
+            currentNode = currentNode.next;
+            currentIdx++;
+        }
+    
+        if (currentNode) {
+            if (currentNode.prev) {
+                currentNode.prev.next = currentNode.next;
+            } else {
+                this.head = currentNode.next;
+            }
+    
+            if (currentNode.next) {
+                currentNode.next.prev = currentNode.prev;
+            } else {
+                this.tail = currentNode.prev;
+            }
+    
+            this.length--;
+            return currentNode.value;
+        }
+    
+        return undefined;
+    }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,54 @@
+type Node<T> = {
+    value: T,
+    prev?: Node<T>
+}
+
 export default class Stack<T> {
     public length: number;
-
+    private head?: Node<T>;
     
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
 
-}
+        const node = {value: item} as Node<T>;
+
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+
+    }
     pop(): T | undefined {
 
-}
-    peek(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            // Need the as Node<T> to appease type script?
+            const head = this.head;
+            this.head = undefined;
 
-}
+            return head?.value;
+        }
+
+        // Need the as Node<T> to appease type script?
+        const head = this.head as Node<T>;
+        this.head = head.prev;
+
+        // In more traditional languages you would free up
+        // the head space in memory here
+        
+        return head.value;
+
+    }
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
